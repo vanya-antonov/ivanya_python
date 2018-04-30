5d5fd6205650246237b600aadd071601aafc9cb3
vanya-antonov <vanya.antonov@gmail.com>
2018-04-30 13:16:20 +0000

    GTDB2 - work in progress
    git-svn-id: http://slavanya.com/repos/Programming/python/lib/mylib@2863 3d1251db-7488-1241-a1d3-4351ec8cc808

--- baseutildb.py
+++ baseutildb.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import sys, os, re, shutil
 import MySQLdb
 import MySQLdb.cursors
-from local import DB_ACCOUNTS
 from pprint import pprint
+from random import randint
+
+from mylib.local import DB_ACCOUNTS
 
 class BaseUtilDB:
 	def __init__(self, db_account="default"):
@@ -21,14 +24,35 @@ class BaseUtilDB:
 		self.cur = self.db.cursor(MySQLdb.cursors.DictCursor)
 	
 	#---
-	def exec_SQL_ar(self,sql):
-		self.cur.execute(sql)
+	# https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-execute.html
+	def exec_sql_ar(self, sql, *params):
+		self.cur.execute(sql, params)
 		return self.cur.fetchall()
+	
+	#---
+	def exec_sql_nr(self, sql, *params):
+		self.cur.execute(sql, params)
+	
+	#---
+	# Useful for INSERT statements: the sql should be truncated like 'INSERT INTO users (id, name, descr)'
+	def exec_sql_in(self, sql, *params):
+		# Make a list by repearing the '%s' string: https://stackoverflow.com/a/3459131/310453
+		tail_l = ['%s'] * len(params)
+		sql += ' VALUES (' + ','.join(tail_l) + ')'
+		self.cur.execute(sql, params)
+	
+	#---
+	def get_random_db_id(self, table, field):
+		sql = 'SELECT 1 AS count FROM ' + table + ' WHERE ' + field + '=%s'
+		num = randint(10**8, 10**9 - 1)
+		while len(self.exec_sql_ar(sql, num)) > 0:
+			num = randint(10**8, 10**9 - 1)
+		return num
 
 if __name__ == '__main__':
 	print('Hello!!!')
-	budb = BaseUtilDB(db_account='ivdb')
-	all_rows = budb.exec_SQL_ar("SELECT * FROM users limit 2")
+	budb = BaseUtilDB(db_account='gtdb2')
+	all_rows = budb.exec_sql_ar("SELECT * FROM users WHERE id = %s", -2)
 	pprint(all_rows)
 	
 

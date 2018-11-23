f0cf1b58f3dc4c831c65c184da23fb4b827c1278
vanya-antonov <vanya.antonov@gmail.com>
2018-11-23 19:41:38 +0300

    removed comment

---
+++ ivanya/baseutil/sqlalchemy.py
@@ -0,0 +1,25 @@
+
+#import logging, os, re, shutil, sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class BaseAlchemy:
+    """Basic parent class to connect to a database using SQLAlchemy.
+
+    Attributes:
+     - engine - SQLAlchemy engine
+     - s - SQLAlchemy session.
+    """
+
+    def __init__(self, db_info):
+        """Create SQLAlchemy's engine and session.
+
+        Arguments:
+         - db_info - dict with the following keys - host, name, user, pass.
+        """
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+
+        Session = sessionmaker(bind=self.engine)
+        self.s = Session()
+

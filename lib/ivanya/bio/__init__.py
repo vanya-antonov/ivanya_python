97810556700b740f966fbbdb6b48f72128113d97
vanya-antonov <vanya.antonov@gmail.com>
2018-11-28 16:43:54 +0300

    Reorganization of files

---
+++ lib/ivanya/bio/__init__.py
@@ -0,0 +1,36 @@
+
+def get_overlap_region(s1,e1,s2,e2):
+    """0-based system is used (like in Biopython):
+
+    |            INPUT            |          RETURNS            |
+    |-----------------------------|-----------------------------|
+    |                             |                             |
+    |  s1=3      e1=14            |                             |
+    |     |----------|            |        [9,14]               |
+    |           |---------|       |                             |
+    |        s2=9     e2=19       |                             |
+    |                             |                             |
+    """
+    if s1 > e1 or s2 > e2:
+        raise Exception("Something is wrong with the intervals (%i,%i) and (%i,%i)" %
+                        (s1,e1,s2,e2))
+    
+    if s1 <= s2 <= e1 and s1 <= e2 <= e1:
+        # |----------------|
+        #     |--------|
+        return s2, e2
+    elif s2 <= s1 <= e2 and s2 <= e1 <= e2:
+        #     |--------|
+        # |----------------|
+        return s1, e1
+    elif s1 <= s2 <= e1 and s2 <= e1 <= e2:
+        # |------------|
+        #       |-------------|
+        return s2, e1
+    elif s2 <= s1 <= e2 and  s1 <= e2 <= e1:
+        #       |-------------|
+        # |------------|
+        return s1, e2
+    else:
+        return None, None
+

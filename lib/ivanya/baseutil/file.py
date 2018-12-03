03d86e2bce4ebb5264d73d553f22594b087d49c3
vanya-antonov <vanya.antonov@gmail.com>
2018-12-03 16:10:49 +0300

    get_all_ids()  =>  get_all_ints()

--- lib/ivanya/baseutil/file.py
+++ lib/ivanya/baseutil/file.py
@@ -1,27 +1,27 @@
 import fileinput
 
 
-def get_all_ids(fn):
+def get_all_ints(fn):
     """Returns a list  of integers from 'fn'.
-    
+
     Arguments:
-        - fn - a string representing a input file name. If None or equal to '-' - read
-          from STDIN; fn is treated as a single ID if it is a digit string.
+        - fn - a string representing input file name. If None or equal to '-' - read
+          from STDIN; fn is treated as a single input integer if it is a digit.
     """
     if fn is None:
         fn = '-'
-    
+
     if fn.isdigit():
-        return [int(fn)]   # just a single ID
-    
-    all_ids = []
+        return [int(fn)]   # just a single integer
+
+    all_ints = []
     for line in fileinput.input(files=fn):
         line = line.rstrip()
         if line.isspace():
             continue
         if not line.isdigit():
-            raise Exception("Wrong ID '%s'!" % line)
-        all_ids.append(int(line))
-    
-    return all_ids
+            raise Exception("Wrong integer '%s'!" % line)
+        all_ints.append(int(line))
+
+    return all_ints
 

ccc5dd21da35977ea961fd96f00a3dc7e92e469e
vanya-antonov <vanya.antonov@gmail.com>
2018-12-03 18:05:39 +0300

    renamed argument: gc => dna_gc

--- lib/ivanya/biopython/seqrecord.py
+++ lib/ivanya/biopython/seqrecord.py
@@ -6,12 +6,12 @@ from Bio.SeqRecord import SeqRecord
 from ivanya.biopython.seq import make_random_dna_seq
 
 
-def make_random_dna_records(dna_len, num_records, gc=50.0, prefix='rand_'):
+def make_random_dna_records(dna_len, num_records, dna_gc=50.0, prefix='rand_'):
     """Returs a list of SeqRecord objects containing random DNA sequences."""
     all_records = []
     for i in range(num_records):
         record = SeqRecord(
-            seq=make_random_dna_seq(dna_len, gc),
+            seq=make_random_dna_seq(dna_len, dna_gc),
             id=prefix + str(i+1),
             name="",
             description="")
